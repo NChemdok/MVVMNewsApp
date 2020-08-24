6bb18b727672a4aa265c7cadf62dcb2903c84c90
Slowthy Sloth <slothychemdoksloth@gmail.com>
2020-08-24 15:07:40 +0530

    Connected to news Api

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "0c6aa78d99bd48af8a99672c326a4ad9"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
