b39221c09cf5913db31aaa0ae09e0a20297cd4b7
Slowthy Sloth <slothychemdoksloth@gmail.com>
2020-08-30 16:42:49 +0530

    Added Pagination to Recycler View

--- app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDatabase.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDatabase.kt
@@ -6,7 +6,7 @@ import com.androiddevs.mvvmnewsapp.models.Article
 
 @Database(
     entities = [Article::class],
-    version = 1
+    version = 2
 )
 
 @TypeConverters(Converters::class)
@@ -26,6 +26,6 @@ abstract  class ArticleDatabase : RoomDatabase() {
                 context.applicationContext,
                 ArticleDatabase::class.java,
                 "article_db.db"
-            ).build()
+            ).fallbackToDestructiveMigration().build()
     }
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "0c6aa78d99bd48af8a99672c326a4ad9"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
